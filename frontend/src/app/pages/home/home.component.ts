932bef853e47c71695ebfc4c58779e672be9ed22
OrcsikRoland <orcsikroland21@gmail.com>
2025-12-31 00:31:30 +0100

    feat: refactor HomeComponent to use NameFacadeService for API interactions

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NameItem } from '../../models/name-item';
 import { NameService } from '../../services/name.service';
+import { NameFacadeService } from '../../services/name-facade.service';
 
 @Component({
   selector: 'app-home',
@@ -14,39 +15,42 @@ export class HomeComponent {
   picked: NameItem | null = null;
   error: string | null = null;
 
-  constructor(private nameService: NameService) {}
+  constructor(private facade: NameFacadeService) {}
 
   ngOnInit(): void {
     this.load();
   }
 
   load(): void {
-    this.names = this.nameService.getAll();
+    this.facade.getAll().subscribe({
+      next: (res) => this.names = res,
+      error: () => this.error = 'Nem sikerült betölteni a neveket.'
+    });
   }
 
   add(): void {
     this.error = null;
-    try {
-      this.nameService.add(this.newName);
-      this.newName = '';
-      this.load();
-    } catch (e: any) {
-      this.error = e?.message ?? 'Hiba történt.';
-    }
+    this.facade.add(this.newName).subscribe({
+      next: () => {
+        this.newName = '';
+        this.load();
+      },
+      error: (e) => this.error = e?.message ?? 'Hiba történt.'
+    });
   }
 
   remove(id: number): void {
-    this.nameService.remove(id);
-    this.load();
+    this.facade.remove(id).subscribe(() => this.load());
   }
 
   draw(): void {
-    this.picked = this.nameService.random();
+    this.facade.random().subscribe(x => this.picked = x);
   }
 
   clear(): void {
-    this.nameService.clear();
-    this.picked = null;
-    this.load();
+    this.facade.clear().subscribe(() => {
+      this.picked = null;
+      this.load();
+    });
   }
 }
