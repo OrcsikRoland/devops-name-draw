ce287a290f8817cd4392db21782845518881f4e5
OrcsikRoland <orcsikroland21@gmail.com>
2026-01-03 14:01:28 +0100

    feat: add ConfigService to manage backend URL and update NameApiService to use it

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ConfigService } from './config.service';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,12 @@ import { HomeComponent } from './pages/home/home.component';
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [provideHttpClient(), {
+    provide: APP_INITIALIZER,
+    useFactory: (cfg: ConfigService) => () => cfg.load(),
+    deps: [ConfigService],
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/services/name-api.service.ts
+++ frontend/src/app/services/name-api.service.ts
@@ -3,29 +3,28 @@ import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { NameItem } from '../models/name-item';
+import { ConfigService } from '../config.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NameApiService {
 
-  private baseUrl = `${environment.apiBaseUrl}/names`;
-
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private configService: ConfigService) {}
 
     getAll(): Observable<NameItem[]> {
-      return this.http.get<NameItem[]>(this.baseUrl);
+      return this.http.get<NameItem[]>(this.configService.cfg.backendUrl);
     }
 
     add(value: string): Observable<void> {
-      return this.http.post<void>(this.baseUrl, { value });
+      return this.http.post<void>(this.configService.cfg.backendUrl, { value });
     }
 
     remove(id: number): Observable<void> {
-      return this.http.delete<void>(`${this.baseUrl}/${id}`);
+      return this.http.delete<void>(`${this.configService.cfg.backendUrl}/${id}`);
     }
 
     random(): Observable<NameItem> {
-      return this.http.get<NameItem>(`${this.baseUrl}/random`);
+      return this.http.get<NameItem>(`${this.configService.cfg.backendUrl}/random`);
   }
 }
